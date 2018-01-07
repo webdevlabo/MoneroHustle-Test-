2f957f2b579cf6b24bc7316d47821219ba044a2d
webdevlabo <webdevlabo@gmail.com>
2018-01-07 18:46:43 +0200

    New

--- executes/main.sh
+++ executes/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-value=$(</var/new/MoneroHustle/executes/mywallet.txt)
+value=$(</var/new/MoneroHustle-Test-/executes/mywallet.txt)
 echo "$value"
 
-sudo /var/new/MoneroHustle/build/bin/xmr-stak -o stratum+tcp://pool.minexmr.com:7777 -u "$value" -p x --currency monero
+sudo /var/new/MoneroHustle-Test-/build/bin/xmr-stak -o stratum+tcp://pool.minexmr.com:7777 -u "$value" -p x --currency monero
--- savewallet.sh
+++ savewallet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash  
 echo "Let's start MoneroHustle!"
 read -p "Enter your wallet number: " szPassword
-echo "$szPassword" >> /var/new/MoneroHustle/executes/mywallet.txt
+echo "$szPassword" >> /var/new/MoneroHustle-Test-/executes/mywallet.txt
